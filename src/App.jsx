2bcf14fa4e9129a40ebf8234d23b4bb5fb59fffe
Vinicius Willian <viniciuz.dev@outlook.com>
2026-02-13 16:48:42 -0400

    feat: integrate Tailwind CSS and fetch movie data
    - Added Tailwind CSS as a dependency and configured it in vite.config.js.
    - Removed App.css and migrated styles to Tailwind CSS classes.
    - Updated App.jsx to fetch popular movies from The Movie Database API and display them using MovieCard component.
    - Modified MovieCard component to accept props for title, description, and poster.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 import MovieCard from './assets/components/MovieCard';
 
 function App() {
-  return <MovieCard />;
+  const [filmes, setFilmes] = useState([]);
+
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_KEY_MOVIES}`;
+
+  useEffect(() => {
+    const buscarFilmes = async () => {
+      try {
+        const response = await fetch(URL);
+
+        if (!response.ok) {
+          throw new Error('Erro ao buscar filmes');
+        }
+
+        const data = await response.json();
+        setFilmes(data.results);
+      } catch (error) {
+        console.error('Erro na API:', error);
+      }
+    };
+
+    buscarFilmes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de filmes</h1>
+      <ul className="flex gap-2 flex-wrap ">
+        {filmes.map((filme) => (
+          <MovieCard
+            key={filme.id}
+            title={filme.original_title}
+            description={filme.overview}
+            poster={filme.poster_path}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/assets/components/MovieCard.jsx
+++ src/assets/components/MovieCard.jsx
@@ -1,8 +1,11 @@
-
-const MovieCard = () => {
+const MovieCard = ({ title, description, poster }) => {
   return (
-    <div>MovieCard</div>
-  )
-}
+    <li className="w-2xs">
+      <img src={`https://image.tmdb.org/t/p/w500/${poster}`} alt="" />
+      <h1 className="text-3xl font-bold underline">{title}</h1>
+      <p>{description}</p>
+    </li>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
